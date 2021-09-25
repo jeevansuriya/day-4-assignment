81cfc3f3aaff1c082ebc104fd3d01a620bfea0c0
Jeevan Suriya.R <91111425+jeevansuriya@users.noreply.github.com>
2021-09-25 18:32:37 +0530

    Add files via upload

---
+++ compare 2 json.js
@@ -0,0 +1,37 @@
+
+
+const readline = require('readline');
+const inp = readline.createInterface({
+  input: process.stdin
+});
+const userInput = [];
+inp.on("line", (data) => {
+ userInput.push(data);
+});
+inp.on("close", () => {
+  
+
+
+var obj1 = {"name":"Sam","class":"MCA"};
+
+var obj2 = {"class":"MCA","name":"Sam"};
+
+var flag=true;
+
+if(Object.keys(obj1).length==Object.keys(obj2).length){
+    for(key in obj1) { 
+        if(obj1[key] == obj2[key]) {
+            continue;
+        }
+        else {
+            flag=false;
+            break;
+        }
+    }
+}
+else {
+    flag=false;
+}
+console.log("is object equal"+flag);
+Share
+});
